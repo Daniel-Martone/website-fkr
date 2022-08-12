a92859d0d84713ace6decc11c9b3ea5efe52daa8
Daniel Martone <danielleandromartone@gmail.com>
2022-08-12 07:51:08 -0300

    Added prompt
    Now whenever you enter a website, you have to type "martone", "websitefkr" or "daniel" to continue

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 for (let i = -6666; i <= 66666; i+=0.666){
     console.log(i*666)
 }
+    
+wordlist = ["martone", "websitefkr", "daniel"];
+
+do {
+  let word = wordlist[Math.floor(Math.random() * 3)];
+  let response = prompt(`Type ${word} to continue`);
+} while (response != word);
